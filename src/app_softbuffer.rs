41d5ef41daee2709318e6857d93e29c0e13292ab
LeonRein <4107480+LeonRein@users.noreply.github.com>
2025-01-17 12:31:46 +0100

    use threadpool to count particles per pixel use crossbream do send pixel buffers to threads works shit

--- src/app_softbuffer.rs
+++ src/app_softbuffer.rs
@@ -1,10 +1,11 @@
-use core::f32;
+use core::{f32, panic};
 use std::cell::RefCell;
 use std::num::NonZeroU32;
 use std::rc::Rc;
 use std::sync::Mutex;
 use std::time::Instant;
 
+use crossbeam::channel::bounded;
 use scoped_threadpool::Pool;
 use softbuffer::{Context, Surface};
 use winit::application::ApplicationHandler;
@@ -13,6 +14,9 @@ use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
 use crate::particles::Particles;
+
+const THREADS: usize = 16;
+const PIXEL_CHUNK_LEN: usize = 1000;
 
 struct AppData {
     window: Rc<Window>,
@@ -31,7 +35,7 @@ struct App {
 
 impl Default for App {
     fn default() -> Self {
-        let threadpool = Rc::new(RefCell::new(Pool::new(16)));
+        let threadpool = Rc::new(RefCell::new(Pool::new(THREADS as u32)));
         let particles = Particles::new(5_000_000, 1280, 720, Rc::clone(&threadpool));
         App {
             data: None,
@@ -64,7 +68,6 @@ impl ApplicationHandler for App {
         };
         event_loop.set_control_flow(ControlFlow::Poll);
 
-        self.n_frame += 1;
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
@@ -84,87 +87,122 @@ impl ApplicationHandler for App {
                 self.mouse_down = state == ElementState::Pressed;
             }
             WindowEvent::RedrawRequested => {
+                self.n_frame += 1;
                 let now = Instant::now();
                 let frametime = now.duration_since(self.last_frame_time);
                 self.last_frame_time = now;
-                if self.n_frame % 100 == 0 {
-                    println!("#{}: FPS = {}", self.n_frame, 1.0 / frametime.as_secs_f32());
-                }
+                // if self.n_frame % 100 == 0 {
+                println!("#{}: FPS = {}", self.n_frame, 1.0 / frametime.as_secs_f32());
+                // }
 
                 let (width, height) = {
                     let size = window.inner_size();
-                    (size.width, size.height)
+                    (size.width as usize, size.height as usize)
                 };
 
                 surface
                     .resize(
-                        NonZeroU32::new(width).unwrap(),
-                        NonZeroU32::new(height).unwrap(),
+                        NonZeroU32::new(width as u32).unwrap(),
+                        NonZeroU32::new(height as u32).unwrap(),
                     )
                     .unwrap();
 
                 self.particles
                     .update(&frametime, self.mouse_pos, self.mouse_down);
 
-                let mut buffer = surface.buffer_mut().unwrap();
-                let n_buffer_chunks = usize::min(10000, buffer.len());
-                let buffer_chunk_size = buffer.len() / n_buffer_chunks;
-
-                for pixel in buffer.iter_mut() {
-                    *pixel = 0;
-                }
-
-                let buffer_chunks = &buffer
-                    .chunks_exact_mut(buffer_chunk_size)
+                let super_mutex_count_buffer = &(0..THREADS)
+                    .map(|_| vec![0_u16; width * height])
                     .map(Mutex::new)
                     .collect::<Vec<_>>();
-                let particles_chunks = self.particles.particles.chunks(16);
+                let (count_buffer_tx, count_buffer_rx) = &bounded::<&Mutex<Vec<u16>>>(THREADS);
+                super_mutex_count_buffer
+                    .iter()
+                    .for_each(|mutex_count_buffer| {
+                        count_buffer_tx.send(mutex_count_buffer).unwrap();
+                    });
+
+                let particles_chunks = self.particles.particles.chunks(THREADS);
 
                 self.threadpool.borrow_mut().scoped(|scope| {
                     for particles_chunk in particles_chunks {
                         scope.execute(move || {
                             for particle in particles_chunk {
                                 if particle.x < 0.0
-                                    || particle.x as usize >= width as usize - 1
+                                    || particle.x as usize >= width - 1
                                     || particle.y < 0.0
-                                    || particle.y as usize >= height as usize - 1
+                                    || particle.y as usize >= height - 1
                                 {
                                     continue;
                                 }
 
-                                let x = particle.x as usize;
-                                let y = particle.y as usize;
-                                let red = particle.x * 255.0 / width as f32;
-                                let green = particle.y * 255.0 / height as f32;
-                                let blue: f32 = (1.0
-                                    - (particle.x / width as f32)
-                                    - (particle.y / height as f32))
-                                    * 255.0;
-
-                                let index = x + y * width as usize;
-                                let Ok(mut buffer_chunk) =
-                                    buffer_chunks[index / buffer_chunk_size].try_lock()
-                                else {
-                                    // println!("could not get lock");
-                                    continue;
-                                };
-
-                                let val = &mut buffer_chunk[index % buffer_chunk_size];
-                                let mut colors = val.to_le_bytes();
-                                if *val == 0 {
-                                    *val = ((red as u32) << 16)
-                                        + ((green as u32) << 8)
-                                        + (blue as u32);
-                                } else {
-                                    colors.iter_mut().take(3).for_each(|c| {
-                                        *c = (*c).saturating_add(2);
-                                    });
-                                    *val = u32::from_le_bytes(colors);
-                                }
+                                let mutex_buffer = count_buffer_rx.recv().unwrap();
+                                let mut buffer = mutex_buffer.lock().unwrap();
+                                buffer[particle.x as usize + particle.y as usize * width] += 1;
+                                let _ = count_buffer_tx.send(mutex_buffer);
                             }
                         });
                     }
                 });
+
+                let mut pixel_buffer = surface.buffer_mut().unwrap();
+                pixel_buffer.iter_mut().for_each(|pixel| *pixel = 0);
+
+                let pixel_buffer_chunks = pixel_buffer
+                    .chunks_exact_mut(PIXEL_CHUNK_LEN)
+                    .collect::<Vec<_>>();
+
+                let super_count_buffer = super_mutex_count_buffer
+                    .iter()
+                    .map(|mutex_count_buffer| mutex_count_buffer.lock().unwrap())
+                    .collect::<Vec<_>>();
+                let super_count_buffer_chunks = &super_count_buffer
+                    .iter()
+                    .map(|count_buffer| {
+                        count_buffer
+                            .chunks_exact(PIXEL_CHUNK_LEN)
+                            .collect::<Vec<_>>()
+                    })
+                    .collect::<Vec<_>>();
+
+                self.threadpool.borrow_mut().scoped(|scope| {
+                    for (i_chunk, pixel_buffer_chunk) in pixel_buffer_chunks.into_iter().enumerate()
+                    {
+                        let mut count_chunks = Vec::new();
+                        for count_buffer_chunks in super_count_buffer_chunks {
+                            count_chunks.push(count_buffer_chunks[i_chunk]);
+                        }
+                        scope.execute(move || {
+                            for (i_pixel, pixel) in pixel_buffer_chunk.iter_mut().enumerate() {
+                                // println!("i_pixel = {}", i_pixel);
+                                // println!("counts.len() = {}", counts.len());
+                                *pixel =
+                                    count_chunks.iter().fold(0_u16, |a, b| a + b[i_pixel]) as u32; // for pixel in pixel_buffer_chunk.iter_mut() {
+                                //     *pixel = 0x00FF00FF;
+                            }
+                        });
+                    }
+                });
+
+                // let x = particle.x as usize;
+                // let y = particle.y as usize;
+                // let red = particle.x * 255.0 / width as f32;
+                // let green = particle.y * 255.0 / height as f32;
+                // let blue: f32 =
+                //     (1.0 - (particle.x / width as f32) - (particle.y / height as f32)) * 255.0;
+
+                // let index = x + y * width as usize;
+
+                // let val = &mut buffer[index];
+                // let mut colors = val.to_le_bytes();
+                // if *val == 0 {
+                //     *val = ((red as u32) << THREADS) + ((green as u32) << 8) + (blue as u32);
+                // } else {
+                //     colors.iter_mut().take(3).for_each(|c| {
+                //         *c = (*c).saturating_add(2);
+                //     });
+                //     *val = u32::from_le_bytes(colors);
+                // }
+
                 // for particle in &self.particles.particles {
                 //     if particle.x < 0.0
                 //         || particle.x >= width as f32
@@ -180,10 +218,10 @@ impl ApplicationHandler for App {
                 //     let blue: f32 =
                 //         (1.0 - (particle.x / width as f32) - (particle.y / height as f32)) * 255.0;
                 //     buffer[x + y * width as usize] =
-                //         ((red as u32) << 16) + ((green as u32) << 8) + (blue as u32);
+                //         ((red as u32) << THREADS) + ((green as u32) << 8) + (blue as u32);
                 // }
 
-                buffer.present().unwrap();
+                pixel_buffer.present().unwrap();
                 window.request_redraw();
             }
             _ => (),
